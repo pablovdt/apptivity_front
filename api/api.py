32b4033644244d90aeff945cf01f8c112196346f
Pablo del Pozo <pablodelpozovdt@gmail.com>
2025-01-14 17:29:48 +0100

    feat: add url in class Api

--- api/api.py
+++ api/api.py
@@ -3,4 +3,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Api:
-    url: str = os.getenv("BACKEND_URL", "http://localhost:8000/")
+    # url: str = os.getenv("BACKEND_URL", "http://localhost:8000/")
+    url: str = os.getenv("BACKEND_URL", "https://apptivity.up.railway.app/")
